5581464562ce9af6a583547753855e44f57291b7
Cali Leibovitz <calileibo@yahoo.com>
2024-07-11 01:15:42 -0400

    Added function to Set Size button

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
 const container = document.querySelector('.sketch-container');
-const userInput = document.querySelector('.userInput');
+let userInput = document.querySelector('.user-input');
+let gridSize = parseInt(userInput.value);
+const size = document.querySelector('.size-button');
 
 
-let gridSize = userInput.value;
-document.addEventListener("DOMContentLoaded", createDefaultGrid());
-console.log(gridSize);
+document.addEventListener("DOMContentLoaded", createGrid(16));
 
-function createDefaultGrid() {
+size.addEventListener("click", function(){
+    userInput = document.querySelector('.user-input');
+    gridSize = parseInt(userInput.value);
+    createGrid(gridSize);
+    console.log('Button Clicked!');
+});
+
+function createGrid(grid) {
     
     container.textContent = ' ';    
-    for (let i = 0; i < 16; i++){
-        const row = document.createElement('div');
-        row.className = 'grid-row';
-        container.appendChild(row);
-        for (let i = 0; i < 16; i++){
-            const box = document.createElement('div');
-            box.className = 'block';
-            row.appendChild(box);
-        }
-    }
-
-}
-
-function createNewGrid(grid){
-    container.textContent = ' ';
     for (let i = 0; i < grid; i++){
         const row = document.createElement('div');
         row.className = 'grid-row';
@@ -34,4 +26,13 @@ function createNewGrid(grid){
             row.appendChild(box);
         }
     }
+
 }
+
+
+
+
+
+console.log(gridSize);
+
+
